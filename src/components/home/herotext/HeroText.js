7da2324f5ea74673b4d16cef0c9ec94511dda60f
baralsaugat <baralsaugat@gmail.com>
2021-10-19 03:13:16 +1100

    profile pic loaded

--- src/components/home/herotext/HeroText.js
+++ src/components/home/herotext/HeroText.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+import profilepic from "../../../assets/profilepic.jpg";
 import "./heroText-style.css";
 
 export const HeroText = () => {
-  return <div className="main">
-      <h1>Hi</h1>
+  return (
+    <div className="main">
+      <h1>Namaste !!!</h1>
       <h2>This is</h2>
-  </div>;
+      <div className="image">
+        <img src={profilepic} alt="profilepic" />
+      </div>
+    </div>
+  );
 };
