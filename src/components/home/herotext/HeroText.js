746749642703c876b0e17dfbe3cd4079baea07b2
baralsaugat <baralsaugat@gmail.com>
2022-02-06 04:34:46 +1100

    animation

--- src/components/home/herotext/HeroText.js
+++ src/components/home/herotext/HeroText.js
@@ -1,28 +1,35 @@
 import React from "react";
 
 import profilepic from "../../../assets/new.jpg";
-import namaste from "../../../assets/namaste.png";
+import car from "../../../assets/car.png";
+import road from "../../../assets/road.png";
 import "./heroText-style.css";
 
 export const HeroText = () => {
   return (
-    <div className="main-hero">
-      <div className="intro-style">
-        <img src={profilepic} alt="profilepic" className="image-file" />
-        <hr />
+    <div className="top-section-hero">
+      <div className="main-hero">
+        <div className="intro-style">
+          <img src={profilepic} alt="profilepic" className="image-file" />
 
-        <div className="intro-text">
-          <h2>Saugat Baral </h2>
-          <h2>Software Developer</h2>
+          <div className="intro-text">
+            <h4>Saugat Baral </h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+
+        <div className="vertical-line"></div>
+
+        <div className="side-text">
+          <h2>Namaste</h2>
+          <p>Thank you, for the visit</p>
+          <p>I am much into Full Stack Development.</p>
         </div>
       </div>
-
-      <div className="vertical-line"></div>
-
-      <div className="side-text">
-        <h1>Namaste</h1>
-        <p>Thank you, for the visit</p>
-        <p>I am much into Full Stack Development.</p>
+      <div>
+        {" "}
+        <img id="car-image" src={car} alt="car" />
+        <img id="road-image" src={road} alt="road" />
       </div>
     </div>
   );
